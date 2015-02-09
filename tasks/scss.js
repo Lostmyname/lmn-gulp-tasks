a133b24a9089f0e22e5800313a4707a2debbbc17
Callum Macrae <callum@macr.ae>
2015-02-09 18:14:32 +0000

    Added fingerprinting to scss and use gulp-sass

--- tasks/scss.js
+++ tasks/scss.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var path = require('path');
+var findNodeModules = require('find-node-modules');
 var rev = require('../lib/rev');
 
 module.exports = function (gulp, plugins, options) {
@@ -11,14 +11,18 @@ module.exports = function (gulp, plugins, options) {
     }
 
     return gulp.src(options.src)
-      .pipe(plugins.compass({
-        css: options.dest,
-        sass: path.dirname(options.src)
+      .pipe(plugins.sass({
+        imagePath: options.imagePath,
+        includePaths: findNodeModules()
       }))
       .on('error', options.onError) // For some reason gulp-plumber doesn't like -compass
       .pipe(plugins.plumber({ errorHandler: options.onError }))
       .pipe(plugins.autoprefixer())
       .pipe(options.minify ? plugins.minifyCss() : plugins.util.noop())
+      .pipe(options.rev ? plugins.fingerprint('rev-manifest.json', {
+        prefix: '/',
+        mode: 'replace'
+      }) : plugins.util.noop())
       .pipe(gulp.dest(options.dest))
       .pipe(rev(gulp, plugins, options));
   };
