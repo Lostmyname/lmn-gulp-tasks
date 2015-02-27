3cf7b60eef608d324fe0677470b1303604c81a60
Callum Macrae <callum@macr.ae>
2015-02-27 14:31:53 +0000

    Added browserify test

---
+++ test/browserify.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var loadLmnTask = require('../');
+
+var fs = require('fs');
+var path = require('path');
+
+var fixtures = path.join(__dirname, 'fixtures/js');
+var fixturesOut = path.join(__dirname, 'fixtures/out');
+
+function getFile(name, slice) {
+  var buffer = fs.readFileSync(name);
+
+  if (slice !== false) {
+    buffer = buffer.slice(0, -1);
+  }
+
+  return buffer;
+}
+
+
+describe('browserify', function () {
+  it('should parse simple js', function (done) {
+    var out = path.join(fixturesOut, 'simple.js');
+    var stream = loadLmnTask('browserify', {
+      src: path.join(fixtures, 'simple.js'),
+      dest: out
+    })();
+
+    stream.on('finish', function () {
+      var file = getFile(out);
+
+      file.length.should.be.within(450, 550);
+      file.toString().should.containEql('test');
+
+      done();
+    });
+  });
+
+  it('should handle requires', function (done) {
+    var out = path.join(fixturesOut, 'require.js');
+    var stream = loadLmnTask('browserify', {
+      src: path.join(fixtures, 'require.js'),
+      dest: out
+    })();
+
+    stream.on('finish', function () {
+      var file = getFile(out);
+
+      file.length.should.be.within(580, 680);
+      file.toString().should.containEql('test');
+
+      done();
+    });
+  });
+});
---
+++ test/fixtures/js/require.js
@@ -0,0 +1,3 @@
+var simple = require('./simple');
+
+console.log(simple);
