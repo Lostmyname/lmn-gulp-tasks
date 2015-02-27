a5f9626d36329a0ad6d5462dde1e049bd526bb81
Callum Macrae <callum@macr.ae>
2015-02-27 16:57:27 +0000

    Added copy and clean tests, and fixed bug in clean

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,6 +4,6 @@ var del = require('del');
 
 module.exports = function (gulp, plugins, options) {
   return function cleanTask(cb) {
-    del(Array.isArray(options) ? options.src : [options.src], cb);
+    del([].concat(options.src), cb);
   };
 };
---
+++ test/copy-and-clean.js
@@ -0,0 +1,70 @@
+/* global getFile */
+
+'use strict';
+
+var loadLmnTask = require('../');
+
+var path = require('path');
+var should = require('should');
+var _ = require('lodash');
+
+var fixtures = path.join(__dirname, 'fixtures/js/**');
+var fixturesOut = path.join(__dirname, 'fixtures/out');
+
+var f = _.partial(path.join, fixturesOut);
+
+// For convenience, we're testing both the copy and clean tasks in this file
+
+describe('copy and clean', function () {
+  it('should copy files', function (done) {
+    var stream = loadLmnTask('copy', {
+      src: fixtures,
+      dest: fixturesOut
+    })();
+
+    stream.on('finish', function () {
+      var file = getFile(f('simple.js'));
+
+      file.length.should.equal(24);
+      file.toString().should.containEql('test');
+
+      done();
+    });
+  });
+
+  it('should clean files', function (done) {
+    loadLmnTask('clean', {
+      src: f('simple.js')
+    })(function (err, files) {
+      should(err).equal(null);
+
+      files.length.should.equal(1);
+
+      should.throws(function () {
+        getFile(f('simple.js'));
+      }, /ENOENT/);
+
+      done();
+    });
+  });
+
+  it('should clean arrays of files', function (done) {
+    loadLmnTask('clean', {
+      src: [f('bad-jscs.js'), f('bad-jshint.js'), f('require.js')]
+    })(function (err, files) {
+      should(err).equal(null);
+
+      files.length.should.equal(3);
+
+      should.throws(function () {
+        getFile(f('bad-jscs.js'));
+      }, /ENOENT/);
+
+      should.throws(function () {
+        getFile(f('bad-jshint.js'));
+      }, /ENOENT/);
+
+      done();
+    });
+  });
+});
