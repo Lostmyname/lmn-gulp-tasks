095a145f07b53d04fca54606b4f650629db94df1
Callum Macrae <callum@macr.ae>
2015-03-03 11:56:06 +0000

    DRY

--- test/optimise-svgs.js
+++ test/optimise-svgs.js
@@ -3,7 +3,11 @@
 'use strict';
 
 var loadLmnTask = require('../');
+var path = require('path');
 var should = require('should');
+var _ = require('lodash');
+
+var p = _.partial(path.join, './test/fixtures');
 
 describe('optimise-svgs', function () {
   before(clean);
@@ -11,13 +15,13 @@ describe('optimise-svgs', function () {
 
   it('shouldnt optimise unchanged svgs', function (done) {
     var stream = loadLmnTask('optimise-svgs', {
-      src: './test/fixtures/svg/logo-148x35.svg',
-      dest: './test/fixtures/svg'
+      src: p('svg/logo-148x35.svg'),
+      dest: p('svg')
     })();
 
     stream.on('finish', function () {
       should.throws(function () {
-        getFile('./test/fixtures/svg/logo-148x35.png');
+        getFile(p('svg/logo-148x35.png'));
       }, /ENOENT/);
 
       done();
@@ -26,13 +30,13 @@ describe('optimise-svgs', function () {
 
   it('should optimise changed svgs', function (done) {
     var stream = loadLmnTask('optimise-svgs', {
-      src: './test/fixtures/svg/logo-148x35.svg',
-      dest: './test/fixtures/out'
+      src: p('svg/logo-148x35.svg'),
+      dest: p('out')
     })();
 
     stream.on('finish', function () {
-      var expectedSvg = getFile('./test/fixtures/svg/logo-148x35-out.svg');
-      var svgOut = getFile('./test/fixtures/out/logo-148x35.svg');
+      var expectedSvg = getFile(p('svg/logo-148x35-out.svg'));
+      var svgOut = getFile(p('out/logo-148x35.svg'));
 
       svgOut.should.eql(expectedSvg);
 
@@ -41,8 +45,8 @@ describe('optimise-svgs', function () {
   });
 
   it('should rasterise optimised svgs', function () {
-    var expectedPng = getFile('./test/fixtures/svg/logo-148x35-out.png');
-    var pngOut = getFile('./test/fixtures/out/logo-148x35.png');
+    var expectedPng = getFile(p('svg/logo-148x35-out.png'));
+    var pngOut = getFile(p('out/logo-148x35.png'));
 
     pngOut.should.eql(expectedPng);
   });
