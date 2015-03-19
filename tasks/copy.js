a28fd407025b6baa587288b05328867b4376d6ff
Barney Fox <barney@lostmy.name>
2015-03-19 17:23:15 +0000

    added flatten option to copy task

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var rev = require('../lib/rev');
+var through = require('through2');
 
 module.exports = function (gulp, plugins, options) {
   return function copyTask() {
     return gulp.src(options.src)
       .pipe(plugins.plumber({ errorHandler: options.onError }))
+      .pipe(options.flatten ? plugins.rename({ dirname: '' }) : through.obj())
       .pipe(gulp.dest(options.dest))
       .pipe(rev(gulp, plugins, options));
   };
