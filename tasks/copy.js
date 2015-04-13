46b13b09d6a8bb4e0c740b4c1d0df54d5a8503c7
Callum Macrae <callum@macr.ae>
2015-04-13 15:26:47 +0100

    added gulp-changed to copy.
    see 9f51797efbc5e0403aa03ade33caf85baf74ffa9

--- tasks/copy.js
+++ tasks/copy.js
@@ -7,6 +7,9 @@ module.exports = function (gulp, plugins, options) {
   return function copyTask() {
     return gulp.src(options.src)
       .pipe(plugins.plumber({ errorHandler: options.onError }))
+      .pipe(plugins.changed(options.dest, {
+        hasChanged: plugins.changed.compareSha1Digest
+      }))
       .pipe(options.flatten ? plugins.rename({ dirname: '' }) : through.obj())
       .pipe(gulp.dest(options.dest))
       .pipe(rev(gulp, plugins, options));
