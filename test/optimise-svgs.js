99fee5f0b1cbc8a996f06be5da26ce741a98b2bb
Callum Macrae <callum@macr.ae>
2015-04-17 15:34:16 +0100

    Gived optimise-svgs rev, and added test.

--- test/optimise-svgs.js
+++ test/optimise-svgs.js
@@ -1,8 +1,9 @@
-/* global clean, getFile, hookStdout */
+/* global clean, getFile */
 
 'use strict';
 
 var loadLmnTask = require('../');
+var fs = require('fs');
 var path = require('path');
 var should = require('should');
 var _ = require('lodash');
@@ -10,50 +11,72 @@ var _ = require('lodash');
 var p = _.partial(path.join, './test/fixtures');
 
 describe('optimise-svgs', function () {
-  before(clean);
+  beforeEach(clean);
   after(clean);
 
   it('shouldnt optimise unchanged svgs', function (done) {
-    var unhook = hookStdout();
-
     var stream = loadLmnTask('optimise-svgs', {
       src: p('svg/logo-148x35.svg'),
       dest: p('svg')
     })();
 
-    stream.on('finish', function () {
+    stream.resume();
+
+    stream.on('end', function () {
       should.throws(function () {
         getFile(p('svg/logo-148x35.png'));
       }, /ENOENT/);
-
-      unhook();
       done();
     });
   });
 
-  it('should optimise changed svgs', function (done) {
-    var unhook = hookStdout();
-
+  it('should optimise and rasterise changed svgs', function (done) {
     var stream = loadLmnTask('optimise-svgs', {
       src: p('svg/logo-148x35.svg'),
       dest: p('out')
     })();
 
-    stream.on('finish', function () {
+    stream.resume();
+
+    stream.on('end', function () {
       var expectedSvg = getFile(p('svg/logo-148x35-out.svg'));
       var svgOut = getFile(p('out/logo-148x35.svg'), false);
 
       svgOut.should.eql(expectedSvg);
 
-      unhook();
+      var expectedPng = getFile(p('svg/logo-148x35-out.png'));
+      var pngOut = getFile(p('out/logo-148x35.png'));
+
+      pngOut.should.eql(expectedPng);
       done();
     });
   });
 
-  it('should rasterise optimised svgs', function () {
-    var expectedPng = getFile(p('svg/logo-148x35-out.png'));
-    var pngOut = getFile(p('out/logo-148x35.png'));
+  it('should rev svgs and png correctly', function (done) {
+    var stream = loadLmnTask('optimise-svgs', {
+      src: p('svg/logo-148x35.svg'),
+      dest: p('out'),
+      rev: true
+    })();
 
-    pngOut.should.eql(expectedPng);
+    stream.resume();
+
+    stream.on('end', function () {
+      var expectedSvg = getFile(p('svg/logo-148x35-out.svg'));
+      var svgOut = getFile(p('out/logo-148x35.svg'), false);
+
+      svgOut.should.eql(expectedSvg);
+
+      var expectedPng = getFile(p('svg/logo-148x35-out.png'));
+      var pngOut = getFile(p('out/logo-148x35.png'));
+
+      pngOut.should.eql(expectedPng);
+
+      fs.readdir(p('out'), function (err, files) {
+        should(err).equal(null);
+        files.length.should.equal(4);
+        done();
+      });
+    });
   });
 });
