3079ae15ec7f7b265d5b7a90a4ecf102cddcbd2a
Callum Macrae <callum@macr.ae>
2015-10-26 13:33:57 +0000

    Fixed error in older versions of node

--- tasks/extract.js
+++ tasks/extract.js
@@ -6,17 +6,12 @@ var rev = require('../lib/rev');
 
 module.exports = function (vinyl, plugins, options) {
   return function () {
-    try {
-      var src = path.join(findup('node_modules/' + options.module), options.src);
-    } catch (e) {
-      if (e.message.indexOf('Received null') !== -1) {
-        throw new Error('Module ' + options.module + ' not found');
-      } else {
-        throw e;
-      }
+    var module = findup('node_modules/' + options.module);
+    if (!module) {
+      throw new Error('Module ' + options.module + ' not found');
     }
 
-    return vinyl.src(src)
+    return vinyl.src(path.join(module, options.src))
       .pipe(plugins.plumber({ errorHandler: options.onError }))
       .pipe(plugins.rename({ dirname: '' }))
       .pipe(vinyl.dest(options.dest))
