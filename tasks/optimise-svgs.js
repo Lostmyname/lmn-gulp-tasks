772049a1b1a81de4315168c87b0144fb19bcc773
Sam Mason <samm@lostmy.name>
2016-10-07 13:52:11 +0100

    Update optimisesvg task to work on node 6

--- tasks/optimise-svgs.js
+++ tasks/optimise-svgs.js
@@ -3,6 +3,8 @@
 var mergeStream = require('merge-stream');
 var through = require('through2');
 var rev = require('../lib/rev');
+var buffer = require('vinyl-buffer');
+var svgToPng = require('lmn-gulp-svg-to-png');
 
 module.exports = function (vinyl, plugins, options) {
   return function () {
@@ -12,11 +14,15 @@ module.exports = function (vinyl, plugins, options) {
       .pipe(plugins.plumber({ errorHandler: options.onError }))
       .pipe(options.flatten ? plugins.rename({ dirname: '' }) : through.obj())
       .pipe(plugins.changed(options.dest))
-      .pipe(plugins.imagemin({ svgoPlugins: [{ removeViewBox: false }] }))
+      .pipe(plugins.imagemin({
+        svgoPlugins: [{ removeViewBox: false }, {removeDoctype: true}, {removeXMLProcInst: true}]
+      }))
       .pipe(vinyl.dest(options.dest));
 
     // Make optimised PNG fallbacks
-    var pngStream = svgStream.pipe(plugins.svg2png())
+    var pngStream = svgStream
+      .pipe(svgToPng({format: 'png'}))
+      .pipe(buffer())
       .pipe(plugins.imagemin({ progressive: true }))
       .pipe(vinyl.dest(options.dest));
 
