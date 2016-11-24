544735e4d55148474723a9bdbadfb6ba062f1142
Sam Mason <samm@lostmy.name>
2016-11-24 10:57:45 +0000

    Update readme and remove die on error option

--- tasks/css-quality.js
+++ tasks/css-quality.js
@@ -2,14 +2,9 @@
 
 module.exports = function (vinyl, plugins, options) {
   return function cssQualityTask() {
-    // Default to false
-    if (options.dieOnError !== true) {
-      options.dieOnError = process.argv.indexOf('--fail') !== -1;
-    }
 
     return vinyl.src(options.src)
       .pipe(plugins.stylelint({
-        failAfterError: options.dieOnError,
         reporters: [
           { formatter: 'string', console: true }
         ]
