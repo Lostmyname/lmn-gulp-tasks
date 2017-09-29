86633cf5a60401ea38a0bb6f2c65cd3b01f53c54
Marc Thomas <hello@mrcthms.com>
2017-09-29 16:06:44 +0100

    Remove lmn-gulp-svg-to-png

--- tasks/optimise-svgs.js
+++ tasks/optimise-svgs.js
@@ -1,32 +1,23 @@
 'use strict';
 
-var mergeStream = require('merge-stream');
 var through = require('through2');
 var rev = require('../lib/rev');
-var buffer = require('vinyl-buffer');
-var svgToPng = require('lmn-gulp-svg-to-png');
 
 module.exports = function (vinyl, plugins, options) {
   return function () {
-
     // Optimise SVGs
-    var svgStream = vinyl.src(options.src)
+    var svgStream = vinyl
+      .src(options.src)
       .pipe(plugins.plumber({ errorHandler: options.onError }))
       .pipe(options.flatten ? plugins.rename({ dirname: '' }) : through.obj())
       .pipe(plugins.changed(options.dest))
-      .pipe(plugins.imagemin({
-        svgoPlugins: [{ removeViewBox: false }, {removeDoctype: true}, {removeXMLProcInst: true}]
-      }))
-      .pipe(vinyl.dest(options.dest));
-
-    // Make optimised PNG fallbacks
-    var pngStream = svgStream
-      .pipe(svgToPng({format: 'png'}))
-      .pipe(buffer())
-      .pipe(plugins.imagemin({ progressive: true }))
-      .pipe(vinyl.dest(options.dest));
-
-    return mergeStream(pngStream, svgStream)
+      .pipe(
+        plugins.imagemin({
+          svgoPlugins: [{ removeViewBox: false }, { removeDoctype: true }, { removeXMLProcInst: true }]
+        })
+      )
+      .pipe(vinyl.dest(options.dest))
       .pipe(rev(vinyl, plugins, options));
+    return svgStream;
   };
 };
